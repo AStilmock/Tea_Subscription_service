f42bcee35eaa09851f6ea86f6c7fa5c74f28fff9
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-09-19 15:45:03 -0600

    route updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :customers, only: [:index]
+      resources :subscriptions, only: [:index] 
+      get "/customers/:id/subscriptions", to: "customer_subscriptions#index"
+      post "/customers/:id/subscriptions/:id", to: "customer_subscriptions#create"
+      patch "/customers/:id/subscriptions/:id", to: "customer_subscriptions#update"
+      delete "/customers/:id/subscriptions/:id", to: "customer_subscriptions#destroy"
+    end
+  end
 end
