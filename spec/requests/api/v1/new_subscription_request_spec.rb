ffc61b2b71889f52ab5b18b08a7e1e20e6e7c478
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-09-19 17:12:02 -0600

    request test setup

--- spec/requests/api/v1/new_subscription_request_spec.rb
+++ spec/requests/api/v1/new_subscription_request_spec.rb
@@ -1,10 +1,12 @@
-requre 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe 'New Tea Subscription Request' do
   describe 'request response path' do
     it 'can create a new tea subscription' do
       customer1 = Customer.create!(first_name: "Billy", last_name: "Bob", email: "billybobsemail@email.com", address: "123 billy bob lane")
-      subscription1 = Subscription.create!(title: "Monthly", price: 10.00, status: "active", frequency: 1)
+      subsciption1 = Subscription.create!(title: "Monthly", price: 10.00, status: 1, frequency: "Monthly")
+      post "/api/v1/customers/#{customer1.id}/subscriptions", params: {customer_id: customer1.id, subscription_id: subsciption1.id}
+      require 'pry'; binding.pry
     end
   end
 end
