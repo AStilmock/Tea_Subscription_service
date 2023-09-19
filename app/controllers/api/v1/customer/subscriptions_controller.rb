1f966cef11a1542cab1dfc0e9a26b5c0487f0d5c
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-09-19 17:46:38 -0600

    updates for request testing

--- app/controllers/api/v1/customer/subscriptions_controller.rb
+++ app/controllers/api/v1/customer/subscriptions_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::Customer::SubscriptionsController < ApplicationController
   def create
     customer_subscription = CustomerSubscription.create!(customer_id: params[:customer_id], subscription_id: params[:subscription_id])
-    render json: customer_subscription, status: 201
-    require 'pry'; binding.pry
+    render json: { "Subscription Data": { customer: customer_subscription.customer, subscription: customer_subscription.subscription }}, status: 201
   end
 end
