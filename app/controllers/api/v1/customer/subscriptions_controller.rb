504114b175ad4a6b8815e2f68f7b32156f24d39b
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-09-20 10:19:08 -0600

    User cancel subscription request response path

--- app/controllers/api/v1/customer/subscriptions_controller.rb
+++ app/controllers/api/v1/customer/subscriptions_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::Customer::SubscriptionsController < ApplicationController
   def create
-    customer_subscription = CustomerSubscription.create!(customer_id: params[:customer_id], subscription_id: params[:subscription_id])
-    render json: { "Subscription Data": { customer: customer_subscription.customer, subscription: customer_subscription.subscription }}, status: 201
+    customer_subscription = CustomerSubscription.create!(customer_id: params[:customer_id], subscription_id: params[:subscription_id], status: 0, frequency: "Monthly" )
+    render json: { "Subscription Data": { customer_subscription: customer_subscription, customer: customer_subscription.customer, subscription: customer_subscription.subscription }}, status: 201
+  end
+
+  def update
+    cust_sub = CustomerSubscription.find(params[:cust_sub_id])
+    cust_sub.update(status: "cancelled")
+    cust_sub.update(frequency: "cancelled")
+    render json: { "Your subscription has been cancelled": { customer_subscription: cust_sub } }, status: 201
   end
 end
--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -1,4 +1,6 @@
 class CustomerSubscription < ApplicationRecord
+  validates_presence_of :status, :frequency
   belongs_to :customer
   belongs_to :subscription
+  enum status: {"active" => 0, "cancelled" => 1}
 end
