6ece60f43f1e28060435ef73fb4098ad9d9c52f2
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-09-20 19:25:05 -0600

    Schema updates - fix DB table names

--- db/migrate/20230920155402_remove_columns_from_subscriptions.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnsFromSubscriptions < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :subscriptions, :status, :integer
-    remove_column :subscriptions, :frequency, :string
-  end
-end
